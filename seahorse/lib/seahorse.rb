b3141d558f4acfcc0bb7ad5fbeff13dea932c254
Matt Muller <mamuller@amazon.com>
2021-04-22 15:38:23 -0700

    Prefix endpoints with endpoint and hostLabel traits

--- seahorse/lib/seahorse.rb
+++ seahorse/lib/seahorse.rb
@@ -6,6 +6,7 @@ require_relative 'seahorse/json/engines'
 
 require_relative 'seahorse/middleware/around_handler'
 require_relative 'seahorse/middleware/build'
+require_relative 'seahorse/middleware/endpoint'
 require_relative 'seahorse/middleware/parse'
 require_relative 'seahorse/middleware/request_handler'
 require_relative 'seahorse/middleware/response_handler'
@@ -13,9 +14,9 @@ require_relative 'seahorse/middleware/retry'
 require_relative 'seahorse/middleware/send'
 require_relative 'seahorse/middleware/sign'
 require_relative 'seahorse/middleware/validate'
+
 require_relative 'seahorse/middleware_builder'
 require_relative 'seahorse/middleware_stack'
-
 require_relative 'seahorse/output'
 require_relative 'seahorse/time_helper'
 require_relative 'seahorse/stubbing/client_stubs'
---
+++ seahorse/spec/seahorse/middleware/endpoint_spec.rb
@@ -0,0 +1,118 @@
+# frozen_string_literal: true
+
+require 'seahorse/middleware/endpoint'
+
+module Seahorse
+  module Middleware
+
+    describe Endpoint do
+      let(:app) { double('app') }
+      let(:disable_host_prefix) { false }
+      let(:host_prefix) { 'foo.' }
+      let(:params) { { foo: 'bar' } }
+
+      subject do
+        Endpoint.new(
+          app,
+          disable_host_prefix: disable_host_prefix,
+          host_prefix: host_prefix,
+          params: params
+        )
+      end
+
+      describe '#call' do
+        let(:url) { 'https://example.com' }
+        let(:request) { Seahorse::HTTP::Request.new(url: url) }
+        let(:response) { Seahorse::HTTP::Response.new }
+        let(:context) { {} }
+
+        it 'prefixes the host then calls the next middleware' do
+          expect(request).to receive(:prefix_host)
+            .with(host_prefix).ordered.and_call_original
+          expect(app).to receive(:call).with(
+            request: request,
+            response: response,
+            context: context
+          ).ordered
+
+          subject.call(
+            request: request,
+            response: response,
+            context: context
+          )
+
+          expect(request.url).to eq('https://foo.example.com')
+        end
+
+        context 'disable_host_prefix is true' do
+          let(:disable_host_prefix) { true }
+
+          it 'does not prefix the host' do
+            expect(app).to receive(:call).with(
+              request: request,
+              response: response,
+              context: context
+            )
+
+            subject.call(
+              request: request,
+              response: response,
+              context: context
+            )
+
+            expect(request.url).to eq(url)
+          end
+        end
+
+        context 'host prefix has labels' do
+          let(:host_prefix) { '{foo}.' }
+
+          it 'populates the label with params' do
+            expect(app).to receive(:call).with(
+              request: request,
+              response: response,
+              context: context
+            )
+
+            subject.call(
+              request: request,
+              response: response,
+              context: context
+            )
+
+            expect(request.url).to eq('https://bar.example.com')
+          end
+
+          context 'params does not have the label' do
+            let(:params) { {} }
+
+            it 'raises an ArgumentError' do
+              expect do
+                subject.call(
+                  request: request,
+                  response: response,
+                  context: context
+                )
+              end.to raise_error(ArgumentError)
+            end
+          end
+
+          context 'params has an empty label' do
+            let(:params) { { foo: '' } }
+
+            it 'raises an ArgumentError' do
+              expect do
+                subject.call(
+                  request: request,
+                  response: response,
+                  context: context
+                )
+              end.to raise_error(ArgumentError)
+            end
+          end
+        end
+      end
+    end
+
+  end
+end
