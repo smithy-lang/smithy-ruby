482de70c6bc828d5411d4ad8bcdd40963d708794
Matt Muller <mamuller@amazon.com>
2021-06-04 10:26:26 -0700

    Fix relative require

--- seahorse/lib/seahorse/middleware.rb
+++ seahorse/lib/seahorse/middleware.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-require_relative 'seahorse/middleware/around_handler'
-require_relative 'seahorse/middleware/build'
-require_relative 'seahorse/middleware/host_prefix'
-require_relative 'seahorse/middleware/parse'
-require_relative 'seahorse/middleware/request_handler'
-require_relative 'seahorse/middleware/response_handler'
-require_relative 'seahorse/middleware/retry'
-require_relative 'seahorse/middleware/send'
-require_relative 'seahorse/middleware/validate'
+require_relative 'middleware/around_handler'
+require_relative 'middleware/build'
+require_relative 'middleware/host_prefix'
+require_relative 'middleware/parse'
+require_relative 'middleware/request_handler'
+require_relative 'middleware/response_handler'
+require_relative 'middleware/retry'
+require_relative 'middleware/send'
+require_relative 'middleware/validate'
 
 module Seahorse
   # @api private
