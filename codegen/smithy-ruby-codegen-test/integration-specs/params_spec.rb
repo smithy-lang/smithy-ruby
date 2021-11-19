1f64bae928956ec2971c5cbbe107186ebe0443c3
Matt Muller <mamuller@amazon.com>
2021-11-18 20:02:45 -0800

    Attempt to fix test

--- codegen/smithy-ruby-codegen-test/integration-specs/params_spec.rb
+++ codegen/smithy-ruby-codegen-test/integration-specs/params_spec.rb
@@ -3,10 +3,10 @@
 require_relative 'spec_helper'
 
 RSpec.shared_examples "validates types" do |types|
-  let(:shape) { self.class.description }
 
-  it "validates #{shape} is a #{types}" do
+  it "validates input as #{types}" do
     expect do
+      shape = self.class.description
       shape.build({}, context: 'params')
     end.to raise_error(ArgumentError, "Expected params to be in #{types}, got Hash.")
   end
