113f9da2b916a755c18eeeed05d21fbe0ae23e9c
Matt Muller <mamuller@amazon.com>
2021-11-19 09:28:52 -0800

    validate types for all kitchen sink shapes in params

--- codegen/smithy-ruby-codegen-test/integration-specs/params_spec.rb
+++ codegen/smithy-ruby-codegen-test/integration-specs/params_spec.rb
@@ -5,7 +5,7 @@ require_relative 'spec_helper'
 RSpec.shared_examples "validates types" do |*types|
 
   it "validates input as #{types}" do
-    value = types.include?(Hash) ? [] : {}
+    value = types.include?(Hash) ? [] : {} # value can't be nil, so use something that we don't expect
     expect do
       shape = Object.const_get(self.class.description)
       shape.build(value, context: 'params')
@@ -17,7 +17,59 @@ end
 module WhiteLabel
   module Params
     describe EmptyStruct do
-      include_examples "validates types", Hash, WhiteLabel::Types::EmptyStruct
+      include_examples "validates types", Hash, Types::EmptyStruct
+    end
+
+    describe KitchenSink do
+      include_examples "validates types", Hash, Types::KitchenSink
+    end
+
+    describe KitchenSinkOperationInput do
+      include_examples "validates types", Hash, Types::KitchenSinkOperationInput
+    end
+
+    describe ListOfKitchenSinks do
+      include_examples "validates types", Array
+    end
+
+    describe ListOfListOfStrings do
+      include_examples "validates types", Array
+    end
+
+    describe ListOfMapsOfStrings do
+      include_examples "validates types", Array
+    end
+
+    describe ListOfStrings do
+      include_examples "validates types", Array
+    end
+
+    describe ListOfStructs do
+      include_examples "validates types", Array
+    end
+
+    describe MapOfKitchenSinks do
+      include_examples "validates types", Hash
+    end
+
+    describe MapOfMapOfStrings do
+      include_examples "validates types", Hash
+    end
+
+    describe MapOfStrings do
+      include_examples "validates types", Hash
+    end
+
+    describe MapOfStructs do
+      include_examples "validates types", Hash
+    end
+
+    describe SimpleStruct do
+      include_examples "validates types", Hash, Types::SimpleStruct
+    end
+
+    describe StructWithLocationName do
+      include_examples "validates types", Hash, Types::StructWithLocationName
     end
   end
 end
