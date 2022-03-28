d6f13ae6f41e5b7543fc4879aa875cebbf0116d7
Matt Muller <mamuller@amazon.com>
2022-03-28 16:27:29 -0700

    Fix rubocop

--- hearth/lib/hearth/checksums.rb
+++ hearth/lib/hearth/checksums.rb
@@ -11,26 +11,20 @@ module Hearth
     # @return [String<MD5>]
     def self.md5(value)
       if value.is_a?(File) || value.is_a?(Tempfile)
-        OpenSSL::Digest::MD5.file(value).base64digest
+        OpenSSL::Digest.new('MD5').file(value).base64digest
       elsif value.respond_to?(:read)
-        md5 = OpenSSL::Digest::MD5.new
-        update_in_chunks(md5, value)
+        md5 = OpenSSL::Digest.new('MD5')
+        loop do
+          chunk = value.read(CHUNK_SIZE)
+          break unless chunk
+
+          md5.update(chunk)
+        end
+        value.rewind
         md5.base64digest
       else
-        OpenSSL::Digest::MD5.base64digest(value)
+        OpenSSL::Digest.new('MD5').base64digest(value)
       end
-    end
-
-    private
-
-    def self.update_in_chunks(digest, io)
-      loop do
-        chunk = io.read(CHUNK_SIZE)
-        break unless chunk
-
-        digest.update(chunk)
-      end
-      io.rewind unless io.is_a?(IO)
     end
   end
 end
