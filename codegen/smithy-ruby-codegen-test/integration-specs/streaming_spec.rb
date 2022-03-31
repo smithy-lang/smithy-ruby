2c7829631d7bc2de7d6d0d8715685ebc11144063
Alex Woods <alexwoo@amazon.com>
2022-03-31 13:52:23 -0700

    Update specs

--- codegen/smithy-ruby-codegen-test/integration-specs/streaming_spec.rb
+++ codegen/smithy-ruby-codegen-test/integration-specs/streaming_spec.rb
@@ -3,15 +3,21 @@ require_relative 'spec_helper'
 
 module WhiteLabel
   describe Client do
-
     describe '#streaming_operation' do
-      context 'block is provided' do
-        let(:block_io) { double("BlockIO") }
-        it 'creates and uses a blockIO as the body' do
-          client = Client.new(stub_responses: true)
+      let(:client) { Client.new(stub_responses:  true) }
+      let(:output) { 'test' }
 
+      before do
+        client.stub_responses(:streaming_operation, { output: output} )
+      end
+
+      context 'block is provided' do
+
+        let(:block_io) { double("BlockIO") }
+
+        it 'creates and uses a blockIO as the body' do
           expect(Hearth::BlockIO).to receive(:new).and_return(block_io)
-          expect(block_io).to receive(:write).and_return(0)
+          expect(block_io).to receive(:write).with(output).and_return(0)
 
           expect(Hearth::HTTP::Response)
             .to receive(:new)
@@ -25,14 +31,13 @@ module WhiteLabel
       context 'output_stream is set' do
         let(:output_stream) { double("OutputStream") }
         it 'uses the output_stream as the body' do
-          client = Client.new(stub_responses: true)
 
           expect(Hearth::HTTP::Response)
             .to receive(:new)
                   .with(hash_including(body: output_stream))
                   .and_call_original
 
-          expect(output_stream).to receive(:write).and_return(0)
+          expect(output_stream).to receive(:write).with(output).and_return(0)
           client.streaming_operation({}, output_stream: output_stream)
         end
       end
