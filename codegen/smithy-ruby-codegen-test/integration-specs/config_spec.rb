7b4b4d012dd6b3ca383cf7d48ed91261a19ebb36
Matt Muller <mamuller@amazon.com>
2022-05-12 15:57:59 -0700

    Add test case for empty config values

--- codegen/smithy-ruby-codegen-test/integration-specs/config_spec.rb
+++ codegen/smithy-ruby-codegen-test/integration-specs/config_spec.rb
@@ -5,14 +5,14 @@ require_relative 'spec_helper'
 module WhiteLabel
   describe Config do
     describe '#build' do
-      it 'sets member values with defaults' do
+      it 'sets member values' do
         config_keys = {
           adaptive_retry_wait_to_fill: false,
           disable_host_prefix: true,
           endpoint: 'test',
           http_wire_trace: true,
           log_level: :debug,
-          # Logger should exist with log level
+          logger: Logger.new($stdout, level: :debug),
           max_attempts: 0,
           retry_mode: 'adaptive',
           stub_responses: false,
@@ -24,9 +24,12 @@ module WhiteLabel
         config_keys.each do |key, value|
           expect(config.send(key)).to eq(value)
         end
+      end
 
+      it 'uses defaults' do
+        config = Config.new
         expect(config.logger).to be_a(Logger)
-        expect(config.logger.debug?).to be true
+        expect(config.logger.info?).to be true
       end
 
       it 'validates types' do
