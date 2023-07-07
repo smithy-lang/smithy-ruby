21558b6cedfa1fa2ad121768425a14eaafe427a6
Juli Tera <terajul@amazon.com>
2023-07-07 16:16:34 -0400

    Update codegen file with a base outline

--- codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/RequestCompression.java
+++ codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/RequestCompression.java
@@ -1,4 +1,17 @@
 package software.amazon.smithy.ruby.codegen;
 
+import software.amazon.smithy.model.shapes.ServiceShape;
+import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.traits.RequestCompressionTrait;
+import software.amazon.smithy.ruby.codegen.middleware.MiddlewareBuilder;
+
 public class RequestCompression implements RubyIntegration {
+    @Override
+    public boolean includeFor(ServiceShape service, Model model) {
+        return model.isTraitApplied(RequestCompressionTrait.class);
+    }
+
+    @Override
+    public void modifyClientMiddleware(MiddlewareBuilder middlewareBuilder, GenerationContext context){
+    }
 }
