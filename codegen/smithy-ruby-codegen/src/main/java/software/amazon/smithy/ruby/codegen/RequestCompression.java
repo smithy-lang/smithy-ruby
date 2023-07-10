9573c5f9a21ddaec99a5d57404ca714d4ccba4dd
Juli Tera <terajul@amazon.com>
2023-07-10 10:45:01 -0400

    Add configs to codegen middleware

--- codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/RequestCompression.java
+++ codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/RequestCompression.java
@@ -3,6 +3,8 @@ package software.amazon.smithy.ruby.codegen;
 import software.amazon.smithy.model.shapes.ServiceShape;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.traits.RequestCompressionTrait;
+import software.amazon.smithy.ruby.codegen.config.ClientConfig;
+import software.amazon.smithy.ruby.codegen.config.ConfigProviderChain;
 import software.amazon.smithy.ruby.codegen.middleware.MiddlewareBuilder;
 
 public class RequestCompression implements RubyIntegration {
@@ -13,5 +15,33 @@ public class RequestCompression implements RubyIntegration {
 
     @Override
     public void modifyClientMiddleware(MiddlewareBuilder middlewareBuilder, GenerationContext context){
+        ClientConfig disableRequestCompression = (new ClientConfig.Builder())
+                .name("disable_request_compression")
+                .type("Boolean")
+                .defaultValue("false")
+                .documentation("When set to 'true' the request body will not be compressed for supported operations.")
+                .allowOperationOverride()
+                .defaults( new ConfigProviderChain.Builder()
+                        .envProvider("DISABLE_REQUEST_COMPRESSION", "Boolean")
+                        .staticProvider("false")
+                        .build())
+                .build();
+
+        String minCompressionDocumentation = """
+                The minimum size bytes that triggers compression for request bodies.
+                The value must be non-negative integer value between 0 and 10485780 bytes inclusive.
+                """;
+
+        ClientConfig requestMinCompressionSizeBytes = (new ClientConfig.Builder ())
+                .name("request_min_compression_size_bytes")
+                .type("Integer")
+                .defaultValue("10240")
+                .documentation(minCompressionDocumentation)
+                .allowOperationOverride()
+                .defaults( new ConfigProviderChain.Builder()
+                        .envProvider("REQUEST_MIN_COMPRESSION_SIZE_BYTES", "Integer")
+                        .staticProvider("10240")
+                        .build())
+                .build();
     }
 }
