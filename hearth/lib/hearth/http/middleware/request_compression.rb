9996c27b9d2fe605bbd868888720c564154223fe
Juli Tera <terajul@amazon.com>
2023-07-10 11:51:53 -0400

    Create compression file in hearth

---
+++ hearth/lib/hearth/http/middleware/request_compression.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Hearth
+  module HTTP
+    module Middleware
+      # A middleware that compresses the request body and
+      # adds the Content-Encoding header
+      # @api private
+      class RequestCompression
+      end
+    end
+  end
+end
