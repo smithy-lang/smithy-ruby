88162b6cb83ef3d095bd1e21a7dc0d6754cc3eff
Juli Tera <terajul@amazon.com>
2023-07-10 11:47:47 -0400

    Update codegen file with compression middleware

--- codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/RequestCompression.java
+++ codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/RequestCompression.java
@@ -2,17 +2,24 @@ package software.amazon.smithy.ruby.codegen;
 
 import software.amazon.smithy.model.shapes.ServiceShape;
 import software.amazon.smithy.model.Model;
+import software.amazon.smithy.model.shapes.Shape;
 import software.amazon.smithy.model.traits.RequestCompressionTrait;
 import software.amazon.smithy.ruby.codegen.config.ClientConfig;
 import software.amazon.smithy.ruby.codegen.config.ConfigProviderChain;
+import software.amazon.smithy.ruby.codegen.middleware.Middleware;
 import software.amazon.smithy.ruby.codegen.middleware.MiddlewareBuilder;
+import software.amazon.smithy.ruby.codegen.middleware.MiddlewareStackStep;
+import software.amazon.smithy.ruby.codegen.util.Streaming;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class RequestCompression implements RubyIntegration {
     @Override
     public boolean includeFor(ServiceShape service, Model model) {
         return model.isTraitApplied(RequestCompressionTrait.class);
     }
-
     @Override
     public void modifyClientMiddleware(MiddlewareBuilder middlewareBuilder, GenerationContext context){
         ClientConfig disableRequestCompression = (new ClientConfig.Builder())
@@ -43,5 +50,35 @@ public class RequestCompression implements RubyIntegration {
                         .staticProvider("10240")
                         .build())
                 .build();
+
+        Middleware compression = (new Middleware.Builder())
+                .operationPredicate(((model, service, operation) -> operation.hasTrait(RequestCompressionTrait.class)))
+                .operationParams((ctx, operation) -> {
+                    Map<String, String> params = new HashMap<>();
+                    RequestCompressionTrait requestCompression = operation.expectTrait(RequestCompressionTrait.class);
+                    Shape inputShape = ctx.model().expectShape(operation.getInputShape());
+
+                    // need a better way to check if 'encodings' is present
+                    // what is the behavior we want if the list is empty?
+                    if (!requestCompression.getEncodings().isEmpty()){
+                        params.put("encodings", "[" + requestCompression
+                                .getEncodings()
+                                .stream()
+                                .map((s) -> "'" + s + "'")
+                                .collect(Collectors.joining(", ")) + "]");
+                    }
+                    if (Streaming.isStreaming(ctx.model(), inputShape)) {
+                        params.put("streaming", "true");
+                    }
+                    return params;
+                })
+                .addConfig(disableRequestCompression)
+                .addConfig(requestMinCompressionSizeBytes)
+                .klass("Hearth::Middleware::Compression")
+                .step(MiddlewareStackStep.BUILD)
+                .relative(new Middleware.Relative(Middleware.Relative.Type.BEFORE,
+                        "Hearth::HTTP::Middleware::ContentMD5"))
+                .build();
+        middlewareBuilder.register(compression);
     }
 }
