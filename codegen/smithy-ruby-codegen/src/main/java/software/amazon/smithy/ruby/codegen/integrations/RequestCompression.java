959bc0033e2bc9123e5b1f401354512d292a26a2
Juli Tera <terajul@amazon.com>
2023-08-02 14:50:11 -0400

    Add constraints for POC testing

--- codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/integrations/RequestCompression.java
+++ codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/integrations/RequestCompression.java
@@ -29,6 +29,7 @@ import software.amazon.smithy.ruby.codegen.GenerationContext;
 import software.amazon.smithy.ruby.codegen.RubyIntegration;
 import software.amazon.smithy.ruby.codegen.config.ClientConfig;
 import software.amazon.smithy.ruby.codegen.config.ConfigProviderChain;
+import software.amazon.smithy.ruby.codegen.config.RangeConstraint;
 import software.amazon.smithy.ruby.codegen.middleware.Middleware;
 import software.amazon.smithy.ruby.codegen.middleware.MiddlewareBuilder;
 import software.amazon.smithy.ruby.codegen.middleware.MiddlewareStackStep;
@@ -66,6 +67,8 @@ public class RequestCompression implements RubyIntegration {
                 .defaultValue("10240")
                 .documentation(minCompressionDocumentation)
                 .allowOperationOverride()
+                .constraint(new RangeConstraint(0, 100))
+                .constraint(new RangeConstraint(20, 20000)) // to see how this renders
                 .defaults(new ConfigProviderChain.Builder()
                         .staticProvider("10240")
                         .build())
