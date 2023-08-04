d85cb2d54936885dd5d18a21ac2a5d6c30b241b3
Matt Muller <mamuller@amazon.com>
2023-08-04 16:44:34 -0400

    Fix messed up stashing

--- hearth/lib/hearth/auth.rb
+++ hearth/lib/hearth/auth.rb
@@ -1,14 +1,10 @@
 # frozen_string_literal: true
 
 require_relative 'auth/anonymous_identity'
-require_relative 'auth/api_key_identity'
-require_relative 'auth/bearer_token_identity'
+require_relative 'auth/http'
 require_relative 'auth/identity'
 require_relative 'auth/identity_resolver'
-require_relative 'auth/login_identity'
-require_relative 'auth/refreshing_identity'
-require_relative 'auth/scheme'
-require_relative 'auth/signer'
+require_relative 'auth/refreshing_identity_resolver'
 
 module Hearth
   # Contains all authentication related code.
