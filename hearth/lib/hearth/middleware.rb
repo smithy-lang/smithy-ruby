eea4252dccc115da84ab4b2463a1a3a37ca007a5
Matt Muller <mamuller@amazon.com>
2023-08-18 14:08:47 -0400

    Remove request and response handler

--- hearth/lib/hearth/middleware.rb
+++ hearth/lib/hearth/middleware.rb
@@ -3,8 +3,6 @@
 require_relative 'middleware/build'
 require_relative 'middleware/host_prefix'
 require_relative 'middleware/parse'
-require_relative 'middleware/request_handler'
-require_relative 'middleware/response_handler'
 require_relative 'middleware/retry'
 require_relative 'middleware/send'
 require_relative 'middleware/validate'
