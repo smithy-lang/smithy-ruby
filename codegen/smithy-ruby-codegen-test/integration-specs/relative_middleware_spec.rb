af27a8119447c4e4c6eb20b2ff209746cfa123c0
Juli Tera <terajul@amazon.com>
2023-08-28 13:41:33 -0700

    Add relative middleware specs

---
+++ codegen/smithy-ruby-codegen-test/integration-specs/relative_middleware_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require_relative 'spec_helper'
+
+module WhiteLabel
+  describe Client do
+    context 'relative middleware ordering' do
+      it 'allows a middleware to be set before their relative' do
+        config = Config.new(
+          stub_responses: true,
+          verify_before_middleware: proc { |metadata|
+            expect(metadata[:before_middleware]).to be(true)
+          }
+        )
+        client = Client.new(config)
+        client.request_compression_operation({ body: 'foo' })
+      end
+
+      it 'allows a middleware to be set after their relative and ' \
+         'a second middleware to be set when their relative is optional' do
+        config = Config.new(
+          stub_responses: true,
+          verify_mid_middleware: proc { |metadata|
+            expect(metadata[:mid_middleware]).to be(true)
+          }
+        )
+        client = Client.new(config)
+        client.request_compression_operation({ body: 'foo' })
+      end
+    end
+  end
+end
