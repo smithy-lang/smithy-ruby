9379d708e7d4656806cfa53bf4a08f7024c9d641
Juli Tera <terajul@amazon.com>
2024-04-25 14:26:02 -0700

    Update header list parser spec

--- hearth/spec/hearth/http/header_list_parser_spec.rb
+++ hearth/spec/hearth/http/header_list_parser_spec.rb
@@ -3,6 +3,40 @@
 module Hearth
   module Http
     describe HeaderListParser do
+      describe '.parse_boolean_list' do
+        it 'parses an empty list' do
+          expect(HeaderListParser.parse_boolean_list(''))
+            .to eq([])
+        end
+
+        it 'parses a single item' do
+          expect(HeaderListParser.parse_boolean_list('true'))
+            .to eq([true])
+        end
+
+        it 'parses multiple items' do
+          expect(HeaderListParser.parse_boolean_list('true, false, true'))
+            .to eq([true, false, true])
+        end
+      end
+
+      describe '.parse_integer_list' do
+        it 'parses an empty list' do
+          expect(HeaderListParser.parse_integer_list(''))
+            .to eq([])
+        end
+
+        it 'parses a single item' do
+          expect(HeaderListParser.parse_integer_list('1'))
+            .to eq([1])
+        end
+
+        it 'parses multiple items' do
+          expect(HeaderListParser.parse_integer_list('1, 2, 3'))
+            .to eq([1, 2, 3])
+        end
+      end
+
       describe '.parser_string_list' do
         it 'parses an empty list' do
           expect(HeaderListParser.parse_string_list(''))
@@ -38,21 +72,77 @@ module Hearth
         end
 
         it 'parses a single item' do
-          expect(HeaderListParser.parse_http_date_list(
-                   'Mon, 16 Dec 2019 23:48:18 GMT'
-                 ))
-            .to eq([Time.parse('Mon, 16 Dec 2019 23:48:18 GMT')])
+          expect(
+            HeaderListParser.parse_http_date_list(
+              'Mon, 16 Dec 2019 23:48:18 GMT'
+            )
+          ).to eq([Time.parse('Mon, 16 Dec 2019 23:48:18 GMT')])
         end
 
         it 'parses multiple  items' do
-          expect(HeaderListParser.parse_http_date_list(
-                   'Mon, 16 Dec 2019 23:48:18 GMT,' \
-                   'Mon, 16 Dec 2019 23:48:18 GMT'
-                 ))
-            .to eq([
-                     Time.parse('Mon, 16 Dec 2019 23:48:18 GMT'),
-                     Time.parse('Mon, 16 Dec 2019 23:48:18 GMT')
-                   ])
+          expect(
+            HeaderListParser.parse_http_date_list(
+              'Mon, 16 Dec 2019 23:48:18 GMT,' \
+              'Mon, 16 Dec 2019 23:48:18 GMT'
+            )
+          ).to eq(
+            [
+              Time.parse('Mon, 16 Dec 2019 23:48:18 GMT'),
+              Time.parse('Mon, 16 Dec 2019 23:48:18 GMT')
+            ]
+          )
+        end
+      end
+
+      describe '.parse_date_time_list' do
+        it 'parses an empty list' do
+          expect(HeaderListParser.parse_date_time_list(''))
+            .to eq([])
+        end
+
+        it 'parses a single item' do
+          expect(
+            HeaderListParser.parse_date_time_list(
+              '1970-01-01T00:00:00Z'
+            )
+          ).to eq([Time.parse('1970-01-01T00:00:00Z')])
+        end
+
+        it 'parses multiple items' do
+          expect(
+            HeaderListParser.parse_date_time_list(
+              '1970-01-01T00:00:00Z,' \
+              '2000-01-02T20:34:56.123Z'
+            )
+          ).to eq(
+            [
+              Time.parse('1970-01-01T00:00:00Z'),
+              Time.parse('2000-01-02T20:34:56.123Z')
+            ]
+          )
+        end
+      end
+
+      describe '.parse_epoch_seconds_list' do
+        it 'parses an empty list' do
+          expect(HeaderListParser.parse_epoch_seconds_list(''))
+            .to eq([])
+        end
+
+        it 'parses a single item' do
+          expect(HeaderListParser.parse_epoch_seconds_list('0.0'))
+            .to eq([Time.at(0.0)])
+        end
+
+        it 'parses multiple items' do
+          expect(
+            HeaderListParser.parse_epoch_seconds_list('0.0, 946845296')
+          ).to eq(
+            [
+              Time.at(0),
+              Time.at(946_845_296)
+            ]
+          )
         end
       end
     end
