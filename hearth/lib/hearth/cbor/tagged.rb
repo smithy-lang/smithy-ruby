98a953e85421988f3efdd638b216bd48a04b73b5
Alex Woods <alexwoo@amazon.com>
2024-05-30 15:58:20 -0700

    Add Hearth CBOR support

---
+++ hearth/lib/hearth/cbor/tagged.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Hearth
+  module Cbor
+    Tagged = Struct.new(:tag, :value) do
+      def to_s
+        "#{tag}(#{value})"
+      end
+
+      def inspect
+        "#{tag}(#{value.inspect})"
+      end
+
+      def data
+        value
+      end
+    end
+  end
+end
