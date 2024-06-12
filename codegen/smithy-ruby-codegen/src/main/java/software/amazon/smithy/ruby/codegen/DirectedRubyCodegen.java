779dd349560371b525758a45e83e21673e67753a
Matt Muller <mamuller@amazon.com>
2024-06-11 21:46:46 -0400

    Move modeled client config fetching to be before integrations

--- codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/DirectedRubyCodegen.java
+++ codegen/smithy-ruby-codegen/src/main/java/software/amazon/smithy/ruby/codegen/DirectedRubyCodegen.java
@@ -145,6 +145,7 @@ public class DirectedRubyCodegen
 
         // Resolve all config
         Set<ClientConfig> unorderedConfig = new HashSet<>();
+        context.getModeledClientConfig().forEach((c) -> c.addToConfigCollection(unorderedConfig));
         context.integrations().forEach((i) -> {
             i.getAdditionalClientConfig(context).forEach((c) -> c.addToConfigCollection(unorderedConfig));
         });
@@ -156,7 +157,6 @@ public class DirectedRubyCodegen
         context.getBuiltInBindingsFromEndpointRules().forEach((b) -> {
             b.getClientConfig().forEach((c) -> c.addToConfigCollection(unorderedConfig));
         });
-        context.getModeledClientConfig().forEach((c) -> c.addToConfigCollection(unorderedConfig));
 
         List<ClientConfig> clientConfigList = unorderedConfig.stream()
                 .sorted(Comparator.comparing(ClientConfig::getName))
