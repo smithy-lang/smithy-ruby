95592feb3815660508def1be5220fbfa92d9fdf4
Alex Woods <alexwoo@amazon.com>
2024-06-13 10:23:56 -0700

    Fix rubocop issues

--- hearth/lib/hearth/cbor/value_matcher.rb
+++ hearth/lib/hearth/cbor/value_matcher.rb
@@ -3,29 +3,43 @@
 require 'rspec/expectations'
 
 # Provides an rspec matcher for CBOR encoded values
+# rubocop:disable Metrics/BlockLength
 RSpec::Matchers.define :match_cbor do |expected|
   match do |actual|
     # identical values don't need more comparison
     return true if actual == expected
 
     expect(actual.class).to eq(expected.class)
+
+    def match_hash(actual, expected)
+      actual.each do |key, value|
+        expect(actual).not_to include(key) unless expected.key?(key)
+
+        match_value(value, expected[key])
+      end
+    end
+
+    def match_array(actual, expected)
+      actual.each_with_index do |value, index|
+        match_value(value, expected[index])
+      end
+    end
+
+    def match_float(actual, expected)
+      return if actual.nan? && expected.nan?
+      return if actual.infinite? && expected.infinite?
+
+      expect(actual).to be_within(0.0001).of(expected)
+    end
+
     def match_value(actual, expected)
       case actual
       when Hash
-        actual.each do |key, value|
-          expect(actual).not_to include(key) unless expected.key?(key)
-
-          match_value(value, expected[key])
-        end
+        match_hash(actual, expected)
       when Array
-        actual.each_with_index do |value, index|
-          match_value(value, expected[index])
-        end
+        match_array(actual, expected)
       when Float
-        return if actual.nan? && expected.nan?
-        return if actual.infinite? && expected.infinite?
-
-        expect(actual).to be_within(0.0001).of(expected)
+        match_float(actual, expected)
       when Time
         expect(actual.utc.iso8601).to eq(expected.utc.iso8601)
       else
@@ -38,3 +52,4 @@ RSpec::Matchers.define :match_cbor do |expected|
 
   diffable
 end
+# rubocop:enable Metrics/BlockLength
