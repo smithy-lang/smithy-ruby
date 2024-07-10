352d4fc9006552b705f4016f6533710f38f4f653
Juli Tera <terajul@amazon.com>
2024-07-10 11:53:53 -0700

    Fix rubocop violation

--- hearth/lib/hearth/telemetry/telemetry_provider.rb
+++ hearth/lib/hearth/telemetry/telemetry_provider.rb
@@ -2,14 +2,13 @@
 
 module Hearth
   module Telemetry
-    # TODO
+    # Base for all TelemetryProvider classes
     class TelemetryProvider
       def initialize(tracer_provider: nil, context_manager: nil)
         @tracer_provider = tracer_provider
         @context_manager = context_manager
       end
-      attr_reader :tracer_provider
-      attr_reader :context_manager
+      attr_reader :tracer_provider, :context_manager
     end
   end
 end
