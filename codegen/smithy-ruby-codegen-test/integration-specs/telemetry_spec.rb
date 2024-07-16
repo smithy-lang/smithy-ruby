0f9325637436a3e5917a2b91510d10113cdda112
Juli Tera <terajul@amazon.com>
2024-07-16 15:33:02 -0700

    Refactor specs

--- codegen/smithy-ruby-codegen-test/integration-specs/telemetry_spec.rb
+++ codegen/smithy-ruby-codegen-test/integration-specs/telemetry_spec.rb
@@ -45,14 +45,35 @@ module WhiteLabel
       let(:otel_provider) { Hearth::Telemetry::OTelProvider.new }
       let(:otel_export) { OpenTelemetry::SDK::Trace::Export }
       let(:otel_exporter) { otel_export::InMemorySpanExporter.new }
-      let(:otel_config_setup) do
+
+      before do
         processor = otel_export::SimpleSpanProcessor.new(otel_exporter)
         OpenTelemetry::SDK.configure do |c|
           c.add_span_processor(processor)
         end
       end
-      let(:client) { Client.new(telemetry_provider: otel_provider) }
-      let(:expected_operation_attributes) do
+
+      let(:client) do
+        Client.new(
+          telemetry_provider: otel_provider,
+          retry_strategy: Hearth::Retry::Standard.new(max_attempts: 0)
+        )
+      end
+
+      let(:body) { 'AAAAA' }
+      let(:finished_send_span) do
+        otel_exporter
+          .finished_spans
+          .find { |span| span.name == 'Middleware.Send' }
+      end
+
+      let(:finished_op_span) do
+        otel_exporter
+          .finished_spans
+          .find { |span| span.name == 'WhiteLabel.TelemetryTest' }
+      end
+
+      let(:expected_op_attrs) do
         {
           'rpc.service' => 'WhiteLabel',
           'rpc.method' => 'TelemetryTest',
@@ -60,7 +81,8 @@ module WhiteLabel
           'code.namespace' => 'WhiteLabel::Client'
         }
       end
-      let(:expected_send_attributes) do
+
+      let(:expected_send_attrs) do
         {
           'http.method' => 'POST',
           'net.protocol.name' => 'http',
@@ -70,7 +92,6 @@ module WhiteLabel
           'http.status_code' => 200
         }
       end
-      let(:body) { 'AAAAA' }
 
       it 'raises error when an otel dependency was not required' do
         allow(Hearth::Telemetry).to receive(:otel_loaded?).and_return(false)
@@ -83,42 +104,77 @@ module WhiteLabel
 
       it 'creates spans with all the supplied parameters' do
         stub_request(:post, 'https://whitelabel.com')
-        otel_config_setup
         client.telemetry_test
-        expect(otel_exporter.finished_spans.count).to eq(2)
 
-        pp otel_exporter.finished_spans
-        child_span = otel_exporter.finished_spans[0]
-        parent_span = otel_exporter.finished_spans[1]
-        expect(child_span.name).to eq('Middleware.Send')
-        expect(child_span.attributes).to eq(expected_send_attributes)
-        expect(child_span.kind).to eq(:internal)
-        expect(parent_span.name).to eq('WhiteLabel.TelemetryTest')
-        expect(parent_span.attributes).to eq(expected_operation_attributes)
-        expect(parent_span.kind).to eq(:client)
-        expect(child_span.parent_span_id).to eq(parent_span.span_id)
+        expect(finished_send_span).not_to be_nil
+        expect(finished_op_span).not_to be_nil
+        expect(finished_send_span.attributes).to eq(expected_send_attrs)
+        expect(finished_op_span.attributes).to eq(expected_op_attrs)
+        expect(finished_send_span.kind).to eq(:internal)
+        expect(finished_op_span.kind).to eq(:client)
+        expect(finished_send_span.parent_span_id)
+          .to eq(finished_op_span.span_id)
       end
 
       it 'applies content-length span attributes when applicable' do
         stub_request(:post, 'https://whitelabel.com')
           .to_return(
-            status: 200,
             body: body,
             headers: { 'Content-Length' => body.size }
           )
+        # these attributes should exist when content-length is involved
+        expected_send_attrs['http.request_content_length'] = body.size.to_s
+        expected_send_attrs['http.response_content_length'] = body.size.to_s
+        client.telemetry_test(body: body)
 
+        expect(finished_send_span.attributes).to eq(expected_send_attrs)
       end
 
       it 'populates span data with error when it occurs' do
+        stub_request(:post, 'https://whitelabel.com')
+          .to_return(status: 500)
+        begin
+          client.telemetry_test
+        rescue StandardError
+          # Ignored
+        end
+        # TODO: need to update span to record_exceptions
       end
 
+      context 'stub_responses' do
+        let(:finished_stub_span) do
+          otel_exporter
+            .finished_spans
+            .find { |span| span.name == 'Middleware.Stub' }
+        end
 
-      context 'stub' do
-        it '' do
+        let(:expected_send_attrs) do
+          {
+            'http.method' => 'POST',
+            'net.protocol.name' => 'http',
+            'net.protocol.version' => '1.1',
+            'net.peer.name' => 'localhost',
+            'net.peer.port' => 80,
+            'http.status_code' => 200
+          }
+        end
 
+        it 'creates spans with all the supplied parameters' do
+          client = Client.new(
+            stub_responses: true,
+            telemetry_provider: otel_provider
+          )
+          client.telemetry_test
+          expect(finished_stub_span).not_to be_nil
+          expect(finished_op_span).not_to be_nil
+          expect(finished_stub_span.attributes).to eq(expected_send_attrs)
+          expect(finished_op_span.attributes).to eq(expected_op_attrs)
+          expect(finished_stub_span.kind).to eq(:internal)
+          expect(finished_op_span.kind).to eq(:client)
+          expect(finished_stub_span.parent_span_id)
+            .to eq(finished_op_span.span_id)
         end
       end
     end
   end
 end
-
