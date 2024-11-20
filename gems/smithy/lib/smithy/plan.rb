64d740e427f15e3033463f010a717f9d1ec0463a
Matt Muller <mamuller@amazon.com>
2024-11-20 12:38:20 -0500

    Option parsing

--- gems/smithy/lib/smithy/plan.rb
+++ gems/smithy/lib/smithy/plan.rb
@@ -1,10 +1,42 @@
 # frozen_string_literal: true
 
+require 'optparse'
+require 'json'
+
 module Smithy
   class Plan
-    def initialize(*args)
-      puts 'Creating a Smithy plan'
-      puts "ARGS: #{args.join(', ')}"
+    def initialize(model)
+      @model = JSON.parse(model)
+      parse_options
+    end
+
+    # @return [Hash] The API model as a JSON hash.
+    attr_reader :model
+
+    # @return [Hash] The command line options.
+    attr_reader :options
+
+    def smith
+      puts 'Run generation here'
+      puts "The model is: #{@model}"
+      puts "Options are: #{@options}"
+      puts "Root directory: #{ENV['SMITHY_ROOT_DIR']}"
+      puts "Output should be written to: #{ENV['SMITHY_PLUGIN_DIR']}"
+      puts "Projection name: #{ENV['SMITHY_PROJECTION_NAME']}"
+      puts "Artifact name: #{ENV['SMITHY_ARTIFACT_NAME']}"
+      puts "Includes prelude: #{ENV['SMITHY_INCLUDES_PRELUDE']}"
+    end
+
+    private
+
+    def parse_options
+      @options = {}
+      OptionParser.new do |opts|
+        opts.banner = "Usage: smithy-ruby [options]"
+        opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+          @options[:verbose] = v
+        end
+      end.parse!
     end
   end
 end
