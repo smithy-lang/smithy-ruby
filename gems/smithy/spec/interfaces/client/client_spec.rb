d3722226d1b1ce063a8b47e1b35c9b5805a00fae
Matt Muller <mamuller@amazon.com>
2024-12-17 19:34:04 -0500

    minor test rename

--- gems/smithy/spec/interfaces/client/client_spec.rb
+++ gems/smithy/spec/interfaces/client/client_spec.rb
@@ -1,32 +1,30 @@
 # frozen_string_literal: true
 
 describe 'Component: Client' do
-  context 'API' do
-    before(:all) do
-      @tmpdir = SpecHelper.generate(['Weather'], :client)
-    end
-
-    after(:all) do
-      SpecHelper.cleanup(['Weather'], @tmpdir)
-    end
-
-    subject { Weather::Client.new(endpoint: 'https://example.com') }
-
-    it 'adds the HTTP plugin when any operation has the http trait' do
-      expect(Weather::Client.plugins).to include(Smithy::Client::Plugins::NetHTTP)
-    end
-
-    it 'has operation methods' do
-      expect(subject).to respond_to(:get_city, :get_current_time, :get_forecast, :list_cities)
-    end
-
-    # it 'builds input for operations' do
-    #   input = subject.send(:build_input, :get_city, { id: 1 })
-    #   expect(input).to be_a(Smithy::Client::Input)
-    # end
-
-    # it 'can call operations' do
-    #   subject.get_city(id: 1)
-    # end
+  before(:all) do
+    @tmpdir = SpecHelper.generate(['Weather'], :client)
   end
+
+  after(:all) do
+    SpecHelper.cleanup(['Weather'], @tmpdir)
+  end
+
+  subject { Weather::Client.new(endpoint: 'https://example.com') }
+
+  it 'adds the HTTP plugin' do
+    expect(Weather::Client.plugins).to include(Smithy::Client::Plugins::NetHTTP)
+  end
+
+  it 'has operation methods' do
+    expect(subject).to respond_to(:get_city, :get_current_time, :get_forecast, :list_cities)
+  end
+
+  # it 'builds input for operations' do
+  #   input = subject.send(:build_input, :get_city, { id: 1 })
+  #   expect(input).to be_a(Smithy::Client::Input)
+  # end
+
+  # it 'can call operations' do
+  #   subject.get_city(id: 1)
+  # end
 end
