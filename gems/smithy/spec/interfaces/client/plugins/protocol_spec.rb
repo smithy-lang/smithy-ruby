668d9b08b2dba225c917c953c37f76611444c17a
Juli Tera <terajul@amazon.com>
2025-02-21 13:50:53 -0800

    Add DummyWeldProtocol

--- gems/smithy/spec/interfaces/client/plugins/protocol_spec.rb
+++ gems/smithy/spec/interfaces/client/plugins/protocol_spec.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
+# TODO: Need to add more test cases once protocol ordering is supported
 describe 'Client: Protocol Plugin', rbs_test: true do
+  # rubocop:disable Lint/UselessAssignment
+  before(:all) do
+    # Define Weld classes (scoped to this block only)
+    DummyProtocol = Class.new
+
+    used = Class.new(Smithy::Weld) do
+      def for?(service)
+        service.keys.first == 'smithy.ruby.tests#ProtocolService'
+      end
+
+      def protocols
+        { 'smithy.ruby.tests#fakeProtocol' => DummyProtocol }
+      end
+    end
+  end
+  # rubocop:enable Lint/UselessAssignment
+
   ['generated client gem', 'generated client from source code'].each do |context|
     next if ENV['SMITHY_RUBY_RBS_TEST'] && context != 'generated client gem'
 
