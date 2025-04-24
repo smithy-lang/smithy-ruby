da01ac9c82ddcfd9d3e29046ffdd6fcd9127cfaf
Matt Muller <mamuller@amazon.com>
2025-04-24 15:38:23 -0400

    Simplify testing dependencies

--- projections/shapes/spec/shapes/protocol_spec.rb
+++ projections/shapes/spec/shapes/protocol_spec.rb
@@ -8,24 +8,5 @@ module ShapeService
     let(:client_options) { { stub_responses: true } }
     let(:client) { Client.new(client_options) }
 
-    # temporary
-    def data_to_hash(obj)
-      case obj
-      when Struct
-        obj.members.each.with_object({}) do |member, hash|
-          value = obj[member]
-          hash[member] = data_to_hash(value) unless value.nil?
-        end
-      when Hash
-        obj.each.with_object({}) do |(key, value), hash|
-          hash[key] = data_to_hash(value)
-        end
-      when Array then obj.collect { |value| data_to_hash(value) }
-      when IO, StringIO then obj.read
-      when Smithy::Schema::Union then obj.to_h
-      else obj
-      end
-    end
-
   end
 end
