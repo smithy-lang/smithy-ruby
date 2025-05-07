8cc697c0a07a211752d7b7d821e675a3ce5a79c6
Juli Tera <terajul@amazon.com>
2025-05-07 13:15:20 -0700

    Remove TimeHelper

--- gems/smithy-schema/lib/smithy-schema.rb
+++ gems/smithy-schema/lib/smithy-schema.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
+require_relative 'smithy-schema/document'
 require_relative 'smithy-schema/shapes'
 require_relative 'smithy-schema/structure'
-require_relative 'smithy-schema/document'
-require_relative 'smithy-schema/time_helper'
 require_relative 'smithy-schema/type_registry'
 require_relative 'smithy-schema/union'
 
