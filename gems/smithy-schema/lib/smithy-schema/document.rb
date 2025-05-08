68c94c16f6e8f940f5d4cf8cfa8e1334ea74932d
Juli Tera <terajul@amazon.com>
2025-05-08 14:40:21 -0700

    Add docs

--- gems/smithy-schema/lib/smithy-schema/document.rb
+++ gems/smithy-schema/lib/smithy-schema/document.rb
@@ -6,25 +6,40 @@ require 'delegate'
 
 module Smithy
   module Schema
+    # The module provides functionality for handling Smithy document types,
+    # which represent protocol-agnostic data structures in the Smithy data model.
+    #
+    # This module includes capabilities for:
+    # * Serialization and deserialization of document data
+    # * Type-aware data handling
+    # * Support for JSON document format
+    #
+    # @example Basic usage with a document
+    #   data = Document::Data.new({ name: "document" })
+    #   data.data  # => { "name" => "example" }
+    #
+    # @example Using with a shape
+    #   shape = Smithy::Schema::StructureShape.new
+    #   data = Document::Data.new({ "name" => "example" }, shape: shape)
+    #
     module Document
-      # A Smithy document type, representing typed or untyped data from Smithy data model.
-      # ## Document types
-      # Document types are protocol-agnostic view of untyped data. They could be combined
-      # with a shape to serialize its contents.
-      #
-      # Smithy-Ruby currently only support JSON documents.
+      # A Smithy document, representing typed or untyped data from the Smithy data model.
+      # The Data class delegates to the underlying data object while providing additional
+      # document-specific functionality.
       class Data < ::SimpleDelegator
-        # @param [Object] data  document data
+        # @param [Object] data  document data that is in JSON-friendly format
         # @param [Hash] options
-        # @option options [Smithy::Schema::StructureShape] :shape shape to reference when setting
-        #  document data.
+        # @option options [String] :discriminator This value is used to identify a specific
+        # shape. This is equivalent of a Smithy shape ID.
         def initialize(data, options = {})
           @data = data
           super(@data)
           @discriminator = options[:discriminator] || nil
         end
 
-        # @return [String] discriminator
+        # Returns the discriminator value for the document
+        #
+        # @return [String. nil] discriminator
         attr_reader :discriminator
 
         def data
