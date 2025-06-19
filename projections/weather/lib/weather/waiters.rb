1664ed71c11f0f457131a365383853ddd8ba3c87
Matt Muller <mamuller@amazon.com>
2025-06-19 11:38:58 -0400

    Add sample weather waiter for review

--- projections/weather/lib/weather/waiters.rb
+++ projections/weather/lib/weather/waiters.rb
@@ -6,5 +6,33 @@ module Weather
 
   # @api private
   module Waiters
+    # @api private
+    #
+    # Waits for a forecast to be available.
+    #
+    class ForecastExists
+      def initialize(options = {})
+        @client = options[:client]
+        @waiter = Smithy::Client::Waiters::Waiter.new(
+          max_wait_time: options[:max_wait_time],
+          min_delay: options[:min_delay] || 2,
+          max_delay: options[:max_delay] || 120,
+          poller: Smithy::Client::Waiters::Poller.new(
+            operation_name: :get_forecast,
+            acceptors: [{
+              "state" => "success",
+              "matcher" => {
+                "success" => true
+              }
+            }]
+          )
+        )
+      end
+
+      def wait(params = {})
+        @waiter.wait(@client, params)
+      end
+    end
+
   end
 end
