3194e7409966f14e949542ab25b0e4df27d7db39
Richard Wang <49211949+richardwang1124@users.noreply.github.com>
2025-07-10 20:33:21 -0700

    User Agent (#310)

---
+++ gems/smithy-client/lib/smithy-client/plugins/user_agent.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+module Smithy
+  module Client
+    module Plugins
+      # @api private
+      class UserAgent < Plugin
+        option(
+          :user_agent_suffix,
+          doc_type: String,
+          docstring: <<~DOCS)
+            An optional string that is appended to the User-Agent header.
+            The default User-Agent includes the smithy-client version,
+            the ruby platform and version, and host OS information.
+          DOCS
+
+        # @api private
+        class Handler < Client::Handler
+          def call(context)
+            context.http_request.headers['User-Agent'] = UserAgent.new(context).to_s
+            @handler.call(context)
+          end
+
+          # @api private
+          class UserAgent
+            def initialize(context)
+              @context = context
+            end
+
+            def to_s
+              ua = "smithy-ruby/#{Smithy::Client::VERSION}"
+              ua += " (#{os_metadata};"
+              ua += " #{language_metadata})"
+              ua += " #{@context.config.user_agent_suffix}" if @context.config.user_agent_suffix
+              ua.strip
+            end
+
+            private
+
+            def os_metadata
+              os =
+                case RbConfig::CONFIG['host_os']
+                when /mac|darwin/
+                  'macos'
+                when /linux|cygwin/
+                  'linux'
+                when /mingw|mswin/
+                  'windows'
+                else
+                  'other'
+                end
+              metadata = os.to_s
+              local_version = Gem::Platform.local.version
+              metadata += " #{local_version}" if local_version
+              metadata + "; #{RbConfig::CONFIG['host_cpu']}"
+            end
+
+            def language_metadata
+              "ruby/#{RUBY_VERSION}"
+            end
+          end
+        end
+
+        handler(Handler, step: :sign, priority: 5)
+      end
+    end
+  end
+end
---
+++ gems/smithy-client/spec/smithy-client/plugins/user_agent_spec.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative '../../spec_helper'
+
+module Smithy
+  module Client
+    module Plugins
+      describe UserAgent do
+        let(:client_class) { ClientHelper.sample_client.const_get(:Client) }
+        let(:client) { client_class.new(stub_responses: true) }
+
+        it 'adds a user agent header to request' do
+          resp = client.operation
+          ua_header = resp.context.http_request.headers['user-agent']
+          expect(ua_header).to_not be_nil
+          expect(ua_header).to include('smithy-ruby')
+          expect(ua_header).to include('macos').or include('linux').or include('windows').or include('other')
+          expect(ua_header).to include('ruby')
+        end
+
+        it 'adds a user agent suffix to user agent string when configured' do
+          client = client_class.new(user_agent_suffix: 'test-suffix', stub_responses: true)
+          resp = client.operation
+          ua_header = resp.context.http_request.headers['user-agent']
+          expect(ua_header).to end_with('test-suffix')
+        end
+      end
+    end
+  end
+end
