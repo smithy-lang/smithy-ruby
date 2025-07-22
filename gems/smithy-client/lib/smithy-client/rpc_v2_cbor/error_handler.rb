dc2c34f9ecfd95b554ec95d08d9f5f407d68a359
Matt Muller <mamuller@amazon.com>
2025-07-22 10:51:22 -0400

    Fix failing tests

--- gems/smithy-client/lib/smithy-client/rpc_v2_cbor/error_handler.rb
+++ gems/smithy-client/lib/smithy-client/rpc_v2_cbor/error_handler.rb
@@ -50,7 +50,7 @@ module Smithy
           context.operation.errors.each do |ref|
             next unless ref.shape.name == code
 
-            data = context.config.cbor_codec.deserialize(ref, body, ref.shape.type.new)
+            data = context.config.cbor_codec.parse(ref, body, ref.shape.type.new)
           end
           data
         end
