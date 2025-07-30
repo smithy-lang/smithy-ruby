094daa6fe1295008e39fd0ba6effd67c571d7089
Richard Wang <wangrch@amazon.com>
2025-07-30 10:11:28 -0700

    Rubocop

--- gems/smithy-client/lib/smithy-client/identity_resolver.rb
+++ gems/smithy-client/lib/smithy-client/identity_resolver.rb
@@ -2,6 +2,8 @@
 
 module Smithy
   module Client
+    # This module provides basic accessors and methods for an
+    # Identity Resolver class which resolves an Identity.
     module IdentityResolver
       # @return [Identity]
       attr_reader :identity
