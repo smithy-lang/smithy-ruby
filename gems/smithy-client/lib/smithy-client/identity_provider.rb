e299e36dfeb92bce4ece7565a6f84ed404d31a76
Richard Wang <wangrch@amazon.com>
2025-08-04 15:13:13 -0700

    Remove set?

--- gems/smithy-client/lib/smithy-client/identity_provider.rb
+++ gems/smithy-client/lib/smithy-client/identity_provider.rb
@@ -5,11 +5,6 @@ module Smithy
     # This module provides basic accessors and methods for an
     # Identity Provider class which provides an Identity.
     module IdentityProvider
-      # @return [Boolean]
-      def set?
-        !@identity.nil?
-      end
-
       # @return [Identity]
       attr_reader :identity
     end
