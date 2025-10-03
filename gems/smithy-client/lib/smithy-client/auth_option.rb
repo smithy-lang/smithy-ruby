33f18e2160ab8105bfb3b1d5ca606bccb2f9fce6
Matt Muller <mamuller@amazon.com>
2025-10-03 10:25:24 -0400

    PR feedback

--- gems/smithy-client/lib/smithy-client/auth_option.rb
+++ gems/smithy-client/lib/smithy-client/auth_option.rb
@@ -4,6 +4,11 @@ module Smithy
   module Client
     # Contains information about candidate authentication schemes.
     class AuthOption
+      # @param [Hash] options
+      # @option options [String] :scheme_id The auth scheme ID.
+      # @option options [Hash] :signer_properties
+      #   Additional properties to pass to the signer.
+      #   This is a hash of String keys to String values and is signer-specific.
       def initialize(options = {})
         @scheme_id = options[:scheme_id]
         @signer_properties = options[:signer_properties] || {}
