26f3a7b100d3fdb3843ff431e24e815d89d74860
Richard Wang <49211949+richardwang1124@users.noreply.github.com>
2025-11-06 08:46:15 -0800

    Update features to use set (#332)

--- gems/smithy-client/spec/smithy-client/features_spec.rb
+++ gems/smithy-client/spec/smithy-client/features_spec.rb
@@ -57,6 +57,17 @@ module Smithy
         end
         expect(Features.tracked).to be_empty
       end
+
+      it 'does not track duplicate features' do
+        Features.track('A', 'B') do
+          expect(Features.tracked).to eq(%w[A B])
+          Features.track('A') do
+            expect(Features.tracked).to eq(%w[A B])
+          end
+          expect(Features.tracked).to eq(%w[A B])
+        end
+        expect(Features.tracked).to be_empty
+      end
     end
   end
 end
