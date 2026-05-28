c47badac287c3dd23e5f13aafc4886076b420038
Juli Tera <57973151+jterapin@users.noreply.github.com>
2026-05-28 09:37:53 -0700

    fix: clamp Retry-After header value to max backoff delay (#338)

--- gems/smithy-client/lib/smithy-client/retry/standard.rb
+++ gems/smithy-client/lib/smithy-client/retry/standard.rb
@@ -38,8 +38,7 @@ module Smithy
           @capacity_amount = @quota.checkout_capacity(error_info)
           return unless @capacity_amount.positive?
 
-          delay = error_info.hints[:retry_after]
-          delay ||= @backoff.call(retry_token.retry_count)
+          delay = compute_delay(error_info, retry_token.retry_count)
           retry_token.retry_count += 1
           retry_token.retry_delay = delay
           retry_token
@@ -48,6 +47,14 @@ module Smithy
         def record_success(retry_token)
           @quota.release(@capacity_amount)
           retry_token
+        end
+
+        private
+
+        def compute_delay(error_info, retry_count)
+          return @backoff.call(retry_count) unless error_info.hints[:retry_after]
+
+          [error_info.hints[:retry_after], @backoff.max_delay].min
         end
       end
     end
