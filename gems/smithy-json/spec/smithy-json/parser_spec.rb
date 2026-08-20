8096673f5914c9172bed1745905b970030490dae
Juli Tera <terajul@amazon.com>
2026-08-20 08:16:03 -0700

    test: drop redundant json null-structure spec

--- gems/smithy-json/spec/smithy-json/parser_spec.rb
+++ gems/smithy-json/spec/smithy-json/parser_spec.rb
@@ -101,11 +101,6 @@ module Smithy
           expect(subject.parse(structure_shape, bytes).to_h).to eq(string: 'string')
         end
 
-        it 'skips members whose wire value is nil' do
-          data = { 'string' => nil }
-          bytes = Json.dump(data)
-          expect(subject.parse(structure_shape, bytes).to_h).to eq({})
-        end
       end
 
       context 'unions' do
