146a8f111279551b208f8d1eb1e57a5cb911709c
Juli Tera <terajul@amazon.com>
2026-08-20 09:24:29 -0700

    test: minor clean ups

--- gems/smithy-xml/spec/smithy-xml/extension_spec.rb
+++ gems/smithy-xml/spec/smithy-xml/extension_spec.rb
@@ -5,7 +5,7 @@ require_relative '../spec_helper'
 module Smithy
   module Xml
     describe Extension do
-      let(:shape) { Schema::Shapes::StructureShape.new(name: 'Structure') }
+      let(:structure) { Schema::Shapes::StructureShape.new(name: 'Structure') }
       let(:element_member) do
         Schema::Shapes::MemberShape.new(
           target: Schema::Shapes::StringShape.new,
@@ -22,20 +22,13 @@ module Smithy
 
       describe '.structure_name' do
         it 'prefers an xmlName trait on the member shape' do
-          member = Schema::Shapes::MemberShape.new(
-            target: Schema::Shapes::StructureShape.new(name: 'Structure'),
-            traits: { xml_name: 'RootElement' }
-          )
+          member = Schema::Shapes::MemberShape.new(target: structure, traits: { xml_name: 'RootElement' })
 
           expect(described_class.structure_name(member)).to eq('RootElement')
         end
 
         it 'falls back to the target structure name' do
-          member = Schema::Shapes::MemberShape.new(
-            target: Schema::Shapes::StructureShape.new(name: 'Structure')
-          )
-
-          expect(described_class.structure_name(member)).to eq('Structure')
+          expect(described_class.structure_name(structure)).to eq('Structure')
         end
       end
 
@@ -57,28 +50,28 @@ module Smithy
 
       describe '.members' do
         it 'returns ordered element and attribute members' do
-          shape.add_member(:string, element_member)
-          shape.add_member(:status, attribute_member)
+          structure.add_member(:string, element_member)
+          structure.add_member(:status, attribute_member)
 
-          expect(described_class.members(shape)).to eq(
+          expect(described_class.members(structure)).to eq(
             elements: [[:string, element_member]],
             attributes: [[:status, attribute_member]]
           )
         end
 
         it 'memoizes the grouped members on shape metadata' do
-          shape.add_member(:string, element_member)
+          structure.add_member(:string, element_member)
 
-          expect(described_class.members(shape)).to be(described_class.members(shape))
+          expect(described_class.members(structure)).to be(described_class.members(structure))
         end
       end
 
       describe '.member_index' do
         it 'indexes structure members by XML member name' do
-          shape.add_member(:string, element_member)
-          shape.add_member(:status, attribute_member)
+          structure.add_member(:string, element_member)
+          structure.add_member(:status, attribute_member)
 
-          expect(described_class.member_index(shape)).to eq(
+          expect(described_class.member_index(structure)).to eq(
             'String' => [:string, element_member],
             'Status' => [:status, attribute_member]
           )
@@ -87,25 +80,22 @@ module Smithy
 
       describe '.namespace_attrs' do
         it 'builds default namespace attrs from xmlNamespace' do
-          shape.traits[:xml_namespace] = { 'uri' => 'http://example.com/ns' }
+          structure.traits[:xml_namespace] = { 'uri' => 'https://example.com/ns' }
 
-          expect(described_class.namespace_attrs(shape)).to eq('xmlns' => 'http://example.com/ns')
-          expect(described_class.namespace_attrs(shape)).to be_frozen
+          expect(described_class.namespace_attrs(structure)).to eq('xmlns' => 'https://example.com/ns')
+          expect(described_class.namespace_attrs(structure)).to be_frozen
         end
 
         it 'builds prefixed namespace attrs from xmlNamespace' do
-          shape.traits[:xml_namespace] = {
-            'uri' => 'http://example.com/ns',
-            'prefix' => 'smithy'
-          }
+          structure.traits[:xml_namespace] = { 'uri' => 'https://example.com/ns', 'prefix' => 'smithy' }
 
-          expect(described_class.namespace_attrs(shape)).to eq('xmlns:smithy' => 'http://example.com/ns')
+          expect(described_class.namespace_attrs(structure)).to eq('xmlns:smithy' => 'https://example.com/ns')
         end
 
         it 'returns a memoized empty hash when no namespace is present' do
-          expect(described_class.namespace_attrs(shape)).to eq({})
-          expect(described_class.namespace_attrs(shape)).to be(described_class.namespace_attrs(shape))
-          expect(described_class.namespace_attrs(shape)).to be_frozen
+          expect(described_class.namespace_attrs(structure)).to eq({})
+          expect(described_class.namespace_attrs(structure)).to be(described_class.namespace_attrs(structure))
+          expect(described_class.namespace_attrs(structure)).to be_frozen
         end
       end
     end
